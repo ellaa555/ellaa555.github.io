241ba7816d06a5412f68ac741d3561fc1fb5028c
ellaa555 <142452464+ellaa555@users.noreply.github.com>
2025-03-03 17:31:32 -0500

    added more clouds and animation

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 /* js for portfolio */
+
+//content visibility
+window.onload = function () {
+    const headers = document.querySelectorAll('.dropdown-head');
+
+    headers.forEach(header => {
+        header.addEventListener('click', function () {
+            const content = this.nextElementSibling; // Get the <p> element right below the header
+
+            // Toggle visibility of content
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    });
+};
+
